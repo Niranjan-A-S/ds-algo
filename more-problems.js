c67511a2d922cbaa2dada718f7822ba9d7a9b485
Niranjan-A-S <niranjan0881@gmail.com>
2023-11-28 10:43:10 +0530

    chore: some extra problems

--- more-problems.js
+++ more-problems.js
@@ -1,4 +1,22 @@
 //!Greatest Common Divisor using Euclidean Algorithm
+function gcd(a, b) {
+    if (a === 0) return b;
+    return gcd(b % a, a);
+}
+// console.log(gcd(3, 4))
+//Big-O = O(log(min(a,b)))
+
+//Another approach
+function euclidGsd(a, b) {
+    if (a == b) return a;
+    else if (a > b) {
+        return euclidGsd(a - b, b);
+    } else {
+        return euclidGsd(a, b - a);
+    }
+}
+console.log(euclidGsd(36, 60))
+
 //!Finding permutations and combinations of a list of numbers
 //! Find the longest common substring in a given string
 //!Knapsack Problem
